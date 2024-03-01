e012a1e1899c874c2fc774b749962c9ce4de2ac9
danidm13 <156891550+danidm13@users.noreply.github.com>
2024-03-01 08:16:16 +0100

    el archivo init bueno

--- initdb.php
+++ initdb.php
@@ -1,14 +1,19 @@
 <?php
+session_start();
 // Configuración de la conexión a la base de datos
-$servername = "httrembogym";
+$servername = "localhost";
 $username = "dani";
 $password = "1234";
 $dbname = "trembogym";
+$port="3308";
 
 // Crear conexión
-$conn = mysqli_connect ($servername, $username, $password);
+$conn = mysqli_connect ($servername, $username, $password, $dbname, $port);
+if (mysqli_connect_errno()) {
+   echo"Failed to connect to  MySQL: ". mysqli_connect_error();
+   exit();
+}
 
-mysqli_select_db ($conn, $dbname);
+mysqli_select_db($conn, $dbname);
 
-mysqli_close ($conn);
-
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
