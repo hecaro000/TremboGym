a05f0386a325d512c4190d963e38d3a83736fb85
hecaro000 <110849086+hecaro000@users.noreply.github.com>
2024-03-08 13:48:20 +0100

    Todo Funcional_Editar Perfil_Log out

--- log_out.php
+++ log_out.php
@@ -1,7 +1,17 @@
 <?php
 
 	require "initdb.php";
-	unset($_SESSION["user_in"]);
-	header('Location: /index.php');
+	if (isset($_SESSION["usuario_in"]))
+	{
+		unset($_SESSION["usuario_in"]);
+
+		$_SESSION["mensaje_ok"] = "Se ha cerrado sesión correctamente";
+		header('Location: index.php');
+	}
+	else
+	{ 
+		$_SESSION["mensaje_error"] = "Ha habido un error al cerrar sesión";
+		header('Location: index.php');
+	}
 
 ?>
